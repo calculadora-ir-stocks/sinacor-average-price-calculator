02182fdb8491472e3b6973cee965829ec356843b
Biscoitinhoo <gustavo.dickmann123@gmail.com>
2024-07-25 16:12:32 -0300

    fix

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import io
+import os
 from correpy.parsers.brokerage_notes.parser_factory import ParserFactory
 
 PDF_PASSWORD = '910'
@@ -10,10 +11,13 @@ class Ticker:
         self.amount = amount
         self.average_traded_price = self.total_cost / self.amount
 
-    def update_average_price(self, total_cost, amount):
+    def update_for_buy(self, total_cost, amount):
         self.total_cost += total_cost
         self.amount += amount
         self.average_traded_price = self.total_cost / self.amount
+
+    def update_for_sell(self, amount):
+        self.amount -= amount
 
 def ticker_exists(ticker, tickers):
     for i in tickers:
@@ -21,25 +25,45 @@ def ticker_exists(ticker, tickers):
 
     return False
 
-def get_ticker_from_list(ticker, tickers):
+def get_ticker_from_list(ticker, tickers) -> Ticker:
     for i in tickers:
-        if (ticker == i): return i
+        if (ticker == i.ticker): return i
      
     return None
 
-with open('files/NotaCorretagem.pdf', 'rb') as f:
-    tickers = []
+# Itera todos os arquivos em /files e calcula o preço médio, o total gasto e a quantidade de todos os ativos negociados
+# e os armazena em tickers[].
+tickers = []
 
-    content = io.BytesIO(f.read())
-    content.seek(0)
-    
-    response = ParserFactory(brokerage_note=content, password=PDF_PASSWORD).parse()
-    transactions = response[0].transactions
+directory = os.fsencode('files/')
+for file in os.listdir(directory):
+    filename = os.fsdecode(file)
+    with open('files/' + filename, 'rb') as f:
 
-    for t in transactions:
-        if (not ticker_exists(t.security.name, tickers)):
-            tickers.append(Ticker(t.security.name, (t.unit_price * t.amount), t.amount))
-            continue
+        content = io.BytesIO(f.read())
+        content.seek(0)
 
-        ticker = get_ticker_from_list(t.security.name, tickers)
-        ticker.update_average_price((t.unit_price * t.amount), t.amount)
+        response = ParserFactory(brokerage_note=content, password=PDF_PASSWORD).parse()
+        transactions = response[0].transactions
+
+        for t in transactions:
+            ticker = get_ticker_from_list(t.security.name, tickers)
+
+            match t.transaction_type.name:
+                case 'BUY':
+                    if (ticker is None):
+                        tickers.append(Ticker(t.security.name, (t.unit_price * t.amount), t.amount))
+                        continue
+                    ticker.update_for_buy((t.unit_price * t.amount), t.amount)
+                case 'SELL':
+                    if (ticker.amount == 0): 
+                        tickers.remove(ticker)
+                        continue
+                    ticker.update_for_sell((t.unit_price * t.amount), t.amount)
+
+for t in tickers:
+    print('\n')
+    print('Ticker: ' + t.ticker)
+    print('Total gasto: ' + str(t.total_cost))
+    print('Quantidade: ' + str(t.amount))
+    print('Preço médio: ' + str(t.average_traded_price))
